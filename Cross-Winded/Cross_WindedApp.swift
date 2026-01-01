836037dd5d47abf3864c598e0a37a914352cb9b6
Trevor Hafner <thafner777lr@gmail.com>
2026-01-01 14:42:57 -0600

    Initial Commit

---
+++ Cross-Winded/Cross_WindedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cross_WindedApp.swift
+//  Cross-Winded
+//
+//  Created by Trevor Hafner on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct Cross_WindedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cross-WindedTests/Cross_WindedTests.swift
@@ -0,0 +1,17 @@
+//
+//  Cross_WindedTests.swift
+//  Cross-WindedTests
+//
+//  Created by Trevor Hafner on 1/1/26.
+//
+
+import Testing
+@testable import Cross_Winded
+
+struct Cross_WindedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
