d960e0af99aba4dc01ca24a46e3862a8085d4445
Trevor Hafner <thafner777lr@gmail.com>
2026-01-01 17:14:44 -0600

    Add ability for metar to be parsed by the Wind format style

--- Cross-Winded/Wind.swift
+++ Cross-Winded/Wind.swift
@@ -6,13 +6,16 @@
 //
 
 import Foundation
+import RegexBuilder
 
 nonisolated struct Wind: Hashable {
     var directionDegrees: Double
     var speedKnots: Double
     var gustKnots: Double? = nil
     
-    struct FormatStyle: Foundation.FormatStyle {
+    struct FormatStyle: ParseableFormatStyle {
+        var parseStrategy: Strategy = Strategy()
+        
         func format(_ value: Wind) -> String {
             let degreeFormat = FloatingPointFormatStyle<Double>.number
                 .rounded(increment: 1)
@@ -27,6 +30,56 @@ nonisolated struct Wind: Hashable {
                 base += "G\(gustKnots.formatted(speedFormat))"
             }
             return base + "KT"
+        }
+        
+        struct Strategy: ParseStrategy {
+            var isLenient = true
+            
+            func parse(_ value: String) throws -> Wind {
+                let regex = Regex {
+                    Capture(Repeat(.digit, count: 3)) { match in
+                        guard let value = Double(match) else {
+                            throw ParseError.unexpectedNonDigitEncountered
+                        }
+                        return value
+                    }
+                    Capture {
+                        Repeat(.digit, 2...3)
+                    } transform: { match in
+                        guard let value = Double(match) else {
+                            throw ParseError.unexpectedNonDigitEncountered
+                        }
+                        return value
+                    }
+                    Optionally {
+                        "G"
+                        Capture {
+                            Repeat(.digit, 2...3)
+                        } transform: { match in
+                            guard let value = Double(match) else {
+                                throw ParseError.unexpectedNonDigitEncountered
+                            }
+                            return value
+                        }
+                    }
+                    Repeat("KT", (isLenient ? 0 : 1)...1)
+                }
+                
+                guard let match = try regex.wholeMatch(in: value) else {
+                    throw ParseError.inputInUnexpectedFormat
+                }
+                
+                let windDirection = match.1
+                let windSpeed = match.2
+                let gustSpeed = match.3
+                
+                return Wind(directionDegrees: windDirection, speedKnots: windSpeed, gustKnots: gustSpeed)
+            }
+            
+            enum ParseError: Error {
+                case unexpectedNonDigitEncountered
+                case inputInUnexpectedFormat
+            }
         }
     }
 }
--- Cross-WindedTests/WindFormatTests.swift
+++ Cross-WindedTests/WindFormatTests.swift
@@ -40,7 +40,92 @@ struct WindFormatTests {
     func formatIntoMETAR(wind: Wind, expectedString: String) async throws {
         #expect(formatStyle.format(wind) == expectedString)
     }
-
+    
+    @Test(arguments:
+            zip(
+                [
+                    "27009KT",
+                    "23014KT",
+                    "01019KT",
+                    "00902KT",
+                    "360400KT",
+                    "00000KT",
+                    "000000KT",
+                    "31015G25KT",
+                    "01421G29KT",
+                    "00315G24KT"
+                ],
+                [
+                    Wind(directionDegrees: 270, speedKnots: 9),
+                    Wind(directionDegrees: 230, speedKnots: 14),
+                    Wind(directionDegrees: 10, speedKnots: 19),
+                    Wind(directionDegrees: 9, speedKnots: 2),
+                    Wind(directionDegrees: 360, speedKnots: 400),
+                    Wind(directionDegrees: 0, speedKnots: 0),
+                    Wind(directionDegrees: 0, speedKnots: 0),
+                    Wind(directionDegrees: 310, speedKnots: 15, gustKnots: 25),
+                    Wind(directionDegrees: 014, speedKnots: 21, gustKnots: 29),
+                    Wind(directionDegrees: 3, speedKnots: 15, gustKnots: 24)
+                ]
+            )
+    )
+    func interpretValidWinds(input: String, expectedInterpretation: Wind) async throws {
+        try #expect(formatStyle.parseStrategy.parse(input) == expectedInterpretation)
+    }
+    
+    @Test(arguments:
+            zip(
+                [
+                    "34504",
+                    "05423",
+                    "004320",
+                    "00000",
+                    "000000",
+                    "23010G34",
+                    "04324G45",
+                    "000100G150"
+                ],
+                [
+                    Wind(directionDegrees: 345, speedKnots: 4),
+                    Wind(directionDegrees: 54, speedKnots: 23),
+                    Wind(directionDegrees: 4, speedKnots: 320),
+                    Wind(directionDegrees: 0, speedKnots: 0),
+                    Wind(directionDegrees: 0, speedKnots: 0),
+                    Wind(directionDegrees: 230, speedKnots: 10, gustKnots: 34),
+                    Wind(directionDegrees: 43, speedKnots: 24, gustKnots: 45),
+                    Wind(directionDegrees: 0, speedKnots: 100, gustKnots: 150)
+                ]
+            )
+    )
+    func lenientValidWindInterprettions(input: String, expectedInterpretation: Wind?) async throws {
+        try #expect(formatStyle.parseStrategy.parse(input) == expectedInterpretation)
+        
+        let strictFormatStyle = Wind.FormatStyle(parseStrategy: Wind.FormatStyle.Strategy(isLenient: false))
+        #expect(throws: Wind.FormatStyle.Strategy.ParseError.inputInUnexpectedFormat) {
+            try strictFormatStyle.parseStrategy.parse(input)
+        }
+    }
+    
+    @Test(arguments:
+            [
+                "3351KT",
+                "3334445KT",
+                "33344G4",
+                "333444G4",
+                "22255G4444",
+                "222555G4444",
+                "2224433",
+                "22244g43",
+                "22244kt",
+                "11133g43KT"
+            ]
+    )
+    func invalidValues(input: String) async throws {
+        #expect(throws: Wind.FormatStyle.Strategy.ParseError.inputInUnexpectedFormat) {
+            try formatStyle.parseStrategy.parse(input)
+        }
+    }
+    
     init() {
         self.formatStyle = Wind.FormatStyle()
     }
